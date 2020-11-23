d9c2fb42138a67ca09658c9dce4d03bbfee3c515
Jean Borges <jeanborges946@gmail.com>
2020-11-23 11:23:47 -0300

    async-await config/axios install

--- src/main.js
+++ src/main.js
@@ -1,1 +1,17 @@
-alert('hello, world');
+import axios from 'axios';
+
+class Api {
+    static async getUserinfo(username) {
+        try{
+            const response = await axios.get(`https://api.github.com/users/${username}`);
+
+            const data = response.data;
+            console.log(data);
+        } catch {
+            console.warn("Invalid Username!!!");
+        }
+    }    
+}
+
+Api.getUserinfo('jeanb762000');
+Api.getUserinfo('jeanb762');
